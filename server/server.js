ba01e95e198e726d88a37c1a27c441a0e799c505
DucAnh220604 <anhducanh133@gmail.com>
2025-08-14 11:20:35 +0700

    first commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
+import connectDB from "./configs/db.js"
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js"
+
+connectDB()
 
 const app = express()
 app.use(cors()) //Enable Cross-Origin Resource Sharing
+
+//Middleware
+app.use(express.json()) //Parse JSON bodies
+app.use(clerkMiddleware()) // Use Clerk middleware
+
+//API to listen to Clerk Webhooks
+app.use("/api/clerk", clerkWebhooks)
+
 app.get('/', (req, res) => {
-  res.send('API is running')
+  res.send('API is running!!!')
 })
 
 const PORT = process.env.PORT || 3000
